ef1c4f67f738b78ac93a36bcd239ad01bdc91554
Astra <139716057+wkeav@users.noreply.github.com>
2026-05-20 14:10:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -153,6 +153,7 @@ document.addEventListener('DOMContentLoaded', function() {
                         <h3 style="color: #333; margin-bottom: 5px;">Computer Scientist Trainee </h3>
                         <p style="color: #FF8C00; margin-bottom: 10px; font-weight: 500;"> 76th Software Engineering Group (SWEG)- Tinker AFB • May 2026-Present</p>
                         <p style="color: #666;"></p>
+                        </div>
                 <div style="display: flex; flex-direction: column; gap: 25px; max-width: 600px; margin: 0 auto; text-align: left;">
                     <div style="padding: 20px; background: #f8f9fa; border-radius: 15px; border: 2px solid #eee;">
                         <h3 style="color: #333; margin-bottom: 5px;">AI Trainer - Data Annotation</h3>
